034ea70af8b1113bebbf30b23a6f68e288e04159
Howon Lee <hlee.howon@gmail.com>
2012-08-27 13:05:41 -0700

    got it to display stuff

--- view.js
+++ view.js
@@ -1,2 +1,7 @@
-//show all the stuff that's going on in the neural network
-var paper = Raphael(document.getElementById("svg"), 300, 300)
+$(document).ready(function(){
+    //show all the stuff that's going on in the neural network
+    var paper = new Raphael(document.getElementById("raphbase"), 300, 300);
+
+    var expsquare = paper.rect(10, 10, 50, 50);
+    expsquare.attr({fill: "red"});
+});
