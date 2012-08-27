05e7cfbc70ae44a35db054321adb39d9a2fd4874
Howon Lee <hlee.howon@gmail.com>
2012-08-27 13:11:11 -0700

    ok, showables are there

--- view.js
+++ view.js
@@ -2,6 +2,10 @@ $(document).ready(function(){
     //show all the stuff that's going on in the neural network
     var paper = new Raphael(document.getElementById("raphbase"), 300, 300);
 
-    var expsquare = paper.rect(10, 10, 50, 50);
-    expsquare.attr({fill: "red"});
+    var expsquare = paper.rect(10, 10, 50, 50)
+                         .attr({fill: "red"})
+                         .data("pool", currbp.pools)
+                         .click(function(){
+                            alert(this.data("pool"));
+                         });
 });
