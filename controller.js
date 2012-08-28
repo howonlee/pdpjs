f82bc8a2ff7bdd358fc62f8ccce00a4ccc798ee1
Howon Lee <hlee.howon@gmail.com>
2012-08-28 12:44:03 -0700

    skeleton added of the controller stuff

--- controller.js
+++ controller.js
@@ -1,1 +1,47 @@
-//not really a controller
+//not really a controller, but it helps to think of it this way
+
+var doReset = function(){
+};
+
+var doNewStart = function(){
+    //just a wrapper for refresh
+    document.location.reload(true);
+};
+
+var doLoadWeights = function(){
+    //nothing so far
+};
+
+var doSaveWeights = function(){
+    //nothing so far
+    //this would require a server
+};
+
+//start the training stuff
+var setTrainOptions = function(){
+
+};
+
+var doTrainStop = function(){
+};
+
+var doTrainRun = function(){
+};
+
+var toTrainStep = function(){
+};
+//end the training stuff
+
+//start the testing stuff
+var setTestOptions = function(){
+};
+
+var doTestStop = function(){
+};
+
+var doTestRun = function(){
+};
+
+var doTestStep = function(){
+};
+//end the testing stuff
